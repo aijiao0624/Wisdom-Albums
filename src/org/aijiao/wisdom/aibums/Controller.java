2cf1088f6daa77daceec42957bfbe0513134e386
lixiaodong <lixiaodong>
2018-10-23 17:14:30 +0800

    文件选择器

--- src/org/aijiao/wisdom/aibums/Controller.java
+++ src/org/aijiao/wisdom/aibums/Controller.java
@@ -2,9 +2,10 @@ package org.aijiao.wisdom.aibums;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.net.URL;
@@ -22,21 +23,42 @@ public class Controller implements Initializable {
      * 打开照片
      */
     public void shouImage() {
-        File file = new File("F:\\应用宝照片备份\\IMG_20170408_160221.jpg");
-        String localUrl = file.toURI().toString();
-        Image localImage = new Image(localUrl, false);
-        ImageView imageView = new ImageView(localImage);
-        imageView.setImage(localImage);
-        imageView.setFitHeight(120);
-        imageView.setFitWidth(120);
-        flowPane.getChildren().addAll(imageView);
+//        File file = new File("F:\\应用宝照片备份\\IMG_20170408_160221.jpg");
+//        String localUrl = file.toURI().toString();
+//        Image localImage = new Image(localUrl, false);
+//        ImageView imageView = new ImageView(localImage);
+//        imageView.setImage(localImage);
+//        imageView.setFitHeight(120);
+//        imageView.setFitWidth(120);
+//        flowPane.getChildren().addAll(imageView);
+        handleOpenImportFile();
+    }
+
+    public void handleOpenImportFile() {
+        DirectoryChooser dc = new DirectoryChooser();
+        File chosenDir = dc.showDialog(flowPane.getScene().getWindow());
+        if (chosenDir == null) {
+            System.out.println("目录不能为空！");
+        }
+        System.out.println("路径：" + chosenDir);
     }
 
     /**
      * 打开目录
      */
     public void openFolder() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+//        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+//        fileChooser.getExtensionFilters().add(extFilter);
+        fileChooser.showOpenDialog(flowPane.getScene().getWindow());
+    }
 
+    // 打开导入窗口
+    // TODO: 2017/5/22
+    public void openDialogImport(MouseEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        File chosenDir = fileChooser.showOpenDialog(flowPane.getScene().getWindow());
     }
 
 }
--- src/org/aijiao/wisdom/aibums/Main.java
+++ src/org/aijiao/wisdom/aibums/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("朕的相册");
+        primaryStage.setTitle("智慧相册");
         primaryStage.setScene(new Scene(root, 800, 550));
         primaryStage.show();
     }
