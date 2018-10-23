52a87b0c7a83648b96ca5d9094f96869aecb580f
lixiaodong <lixiaodong>
2018-10-23 17:54:15 +0800

    扫描图片 加载图片

--- src/org/aijiao/wisdom/aibums/Controller.java
+++ src/org/aijiao/wisdom/aibums/Controller.java
@@ -2,19 +2,24 @@ package org.aijiao.wisdom.aibums;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
     FlowPane flowPane;
+
+    private static List<String> fileList = new ArrayList(); // 当前文件集合
 
     public void initialize(URL location, ResourceBundle resources) {
     }
@@ -41,6 +46,41 @@ public class Controller implements Initializable {
             System.out.println("目录不能为空！");
         }
         System.out.println("路径：" + chosenDir);
+        scanFolder(chosenDir.toString());
+        System.out.println("共扫描到" + fileList.size() + "张照片");
+        //加载照片
+        ImageView imageView = null;
+        Image localImage = null;
+        for (int i = 0; i < fileList.size(); i++) {
+            File file = new File(fileList.get(i));
+            String localUrl = file.toURI().toString();
+            localImage = new Image(localUrl, false);
+            imageView = new ImageView(localImage);
+            imageView.setImage(localImage);
+            imageView.setFitHeight(120);
+            imageView.setFitWidth(120);
+            System.out.println("加载第" + i + "张照片");
+            flowPane.getChildren().addAll(imageView);
+        }
+    }
+
+    /**
+     * 扫描文件夹，获取所有需要处理的文件
+     */
+    private void scanFolder(String filePath) {
+        File file = new File(filePath);
+        if (file.isDirectory()) {
+            String[] fileList = file.list();
+            for (int i = 0; i < fileList.length; i++) {
+                String path = file.getAbsolutePath() + "\\" + fileList[i];
+                scanFolder(path);
+            }
+        } else {
+            if (file.getAbsolutePath().contains(".jpg") || file.getAbsolutePath().contains(".png")) {
+                System.out.println("扫描到的文件：" + file);
+                fileList.add(file.getPath());
+            }
+        }
     }
 
     /**
@@ -54,11 +94,5 @@ public class Controller implements Initializable {
         fileChooser.showOpenDialog(flowPane.getScene().getWindow());
     }
 
-    // 打开导入窗口
-    // TODO: 2017/5/22
-    public void openDialogImport(MouseEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        File chosenDir = fileChooser.showOpenDialog(flowPane.getScene().getWindow());
-    }
 
 }
